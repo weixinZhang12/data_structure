701b221f05472b9167c370eda73c719286a5460d
k7519586.*Z <z2625687838@gmail.com>
2025-01-02 10:46:25 +0800

    modified:   .vscode/launch.json 	modified:   src/b_tree.rs 	modified:   src/main.rs

--- src/b_tree.rs
+++ src/b_tree.rs
@@ -1,46 +1,128 @@
-pub mod b_tree_link{
+pub mod b_tree_link {
     pub struct BTreeNode {
         pub value: i32,
         pub left: Option<Box<BTreeNode>>,
         pub right: Option<Box<BTreeNode>>,
     }
-    
+
     pub struct Btree {
         root: Option<Box<BTreeNode>>,
     }
-    
+
     impl Btree {
         pub fn new() -> Self {
             Btree { root: None }
         }
-        pub fn push(&mut self,value: i32) {
-            let current_node=&mut self.root as * mut Option<Box<BTreeNode>>;
-            while let Some(v) = unsafe { &mut *current_node } {
-                
-            }
+        pub fn push(&mut self, value: i32) {
+            let current_node = &mut self.root as *mut Option<Box<BTreeNode>>;
+            while let Some(v) = unsafe { &mut *current_node } {}
         }
     }
-    
 }
 
-pub mod b_tree{
+pub mod b_tree {
+
+    use colored::Colorize;
+    ///最大索引，实际空间少一个单位。
+    const MAX_INDEX: usize = 15;
     #[derive(Debug)]
-    pub struct Btree{
-        arr:[i32;10]
+    pub struct Btree {
+        arr: [Option<i32>; MAX_INDEX],
     }
     impl Btree {
-        pub fn new()->Self{
-            Btree{
-                arr:[0;10]
+        pub fn new() -> Self {
+            Btree {
+                arr: [None; MAX_INDEX],
             }
         }
-        pub fn push(&mut self,value:i32){
-            for (index,&arr_value) in self.arr.iter().enumerate() {
-                if self.arr[index]==0 {
-                    self.arr[index]=value;
+        fn index_check(&self, index: usize) -> bool {
+            if index >= MAX_INDEX {
+                return true;
+            }
+            false
+        }
+        ///添加元素。如果超出范围，默认放弃此次操作
+        pub fn push(&mut self, value: i32) {
+            self.arr[0] = Some(0);
+            for (index, &arr_value) in self.arr.iter().enumerate() {
+                if self.arr[index] == None {
+                    self.arr[index] = Some(value);
                     return;
                 }
             }
+            eprintln!(
+                "{}",
+                format!(
+                    "错误：索引超出范围，最大容量为{}，无法添加元素",
+                    MAX_INDEX - 1
+                )
+                .red()
+                .bold()
+            )
+        }
+        pub fn get_par(&mut self, index: usize) -> Option<i32> {
+            if index >= MAX_INDEX || index == 0 {
+                if index == 0 {
+                    eprintln!(
+                        "{}",
+                        format!("警告：无效的访问，你不可以访问索引为0的索引")
+                            .yellow()
+                            .bold()
+                    )
+                }
+                if index >= MAX_INDEX {
+                    eprintln!(
+                        "{}",
+                        format!(
+                            "错误：你不可以访问大于容量的索引，当前的最大索引为{}，而你提供的为{}",
+                            MAX_INDEX - 1,
+                            index
+                        )
+                        .red()
+                        .bold()
+                    )
+                }
+                return None;
+            }
+
+            self.arr[index / 2]
+        }
+        ///获取的索引从1开始。如果尝试0索引时返回None，如果超出范围，返回None
+        pub fn get_left(&mut self, index: usize) -> Option<i32> {
+            if index >= MAX_INDEX || index == 0 {
+                if index == 0 {
+                    eprintln!(
+                        "{}",
+                        format!("警告：操作被拒绝，你不能访问index为0的元素，它是受到保护的")
+                            .yellow()
+                            .bold()
+                    )
+                }
+                return None;
+            }
+            let target_index: usize = index * 2;
+            if target_index < MAX_INDEX {
+                return self.arr[target_index];
+            }
+            None
+        }
+        pub fn get_right(&mut self, index: usize) -> Option<i32> {
+            if index >= MAX_INDEX || index == 0 {
+                if index == 0 {
+                    eprintln!(
+                        "{}",
+                        format!("警告：操作被拒绝，你不能访问index为0的元素，它是受到保护的")
+                            .yellow()
+                            .bold()
+                    )
+                }
+                return None;
+            }
+            let target_index: usize = index * 2 + 1;
+            if target_index < MAX_INDEX {
+                return self.arr[target_index];
+            }
+            None
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,24 @@ fn main() {
     tree.push(1);
     tree.push(2);
     tree.push(3);
+    tree.push(4);
+    tree.push(5);
+    tree.push(6);
+    tree.push(7);
+    tree.push(8);
+    tree.push(9);
+    tree.push(10);
+    tree.push(11);
+    tree.push(12);
+    tree.push(13);
+    tree.push(14);
+    tree.push(15);
+    tree.push(16);
+    let a = tree.get_left(1);
+    let index_is_zero = tree.get_right(0);
+    let par = tree.get_par(15);
+    println!("{:?}", a);
+    println!("父节点： {:?}", par);
+    println!("访问index为0的结果： {:?}", index_is_zero);
     println!("{:?}", tree)
 }
